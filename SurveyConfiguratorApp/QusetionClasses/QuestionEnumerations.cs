93ba28f3f5e659f764452a9639813dd83a0ca542
Rami <mhtoutenane119@cit.just.edu.jo>
2020-11-23 14:59:22 +0200

    Use (#region) to format Forms code

---
+++ SurveyConfiguratorApp/QusetionClasses/QuestionEnumerations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyConfiguratorApp
+{
+    /// <summary>
+    /// Enumeration to define types of questions provided by the system
+    /// </summary>
+    public enum QuestionType
+    {
+        Smiley = 1,
+        Slider = 2,
+        Stars = 3
+    }
+}
