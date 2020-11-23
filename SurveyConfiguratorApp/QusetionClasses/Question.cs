d66798c097aef9b19c79f11ef79d99f900d53440
Rami <mhtoutenane119@cit.just.edu.jo>
2020-11-23 14:59:22 +0200

    Create general enumerations file

--- SurveyConfiguratorApp/QusetionClasses/Question.cs
+++ SurveyConfiguratorApp/QusetionClasses/Question.cs
@@ -3,16 +3,6 @@
 namespace SurveyConfiguratorApp
 {
     /// <summary>
-    /// Enumeration to define types of questions provided by the system
-    /// </summary>
-    public enum QuestionType
-    {
-        Smiley = 1,
-        Slider = 2,
-        Stars = 3
-    }
-
-    /// <summary>
     /// Abstract class to represent General question with required attributes
     /// </summary>
     public abstract class Question
