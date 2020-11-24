e295d2846443d890bd3875374b246d2541f51e89
Rami <mhtoutenane119@cit.just.edu.jo>
2020-11-24 16:22:03 +0200

    reorganize project files

---
+++ Survey Configurator Development/SurveyConfiguratorDllProjects/SurveyConfiguratorEntities/DatabaseSettings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace SurveyConfiguratorApp
+{
+    /// <summary>
+    /// Class to represent and store database connection settings
+    /// </summary>
+    public class DatabaseSettings
+    {
+        private string mDatabaseServer;
+        private string mDatabaseName;
+        private string mDatabaseUser;
+        private string mDatabasePassword;
+        /// <summary>
+        /// DatabaseSettings constructor to initialize new DatabaseSettings object
+        /// </summary>
+        /// <param name="databaseServer">Database server</param>
+        /// <param name="databaseName">Database name</param>
+        /// <param name="databaseUser">Database user</param>
+        /// <param name="databasePassword">Database user password</param>
+        public DatabaseSettings(string databaseServer, string databaseName, string databaseUser, string databasePassword)
+        {
+            try
+            {
+                DatabaseServer = databaseServer;
+                DatabaseName = databaseName;
+                DatabaseUser = databaseUser;
+                DatabasePassword = databasePassword;
+            }
+            catch (Exception error)
+            {
+                ErrorLogger.Log(error);
+            }
+        }
+        public string DatabaseServer { get => mDatabaseServer; private set => mDatabaseServer = value; }
+        public string DatabaseName { get => mDatabaseName; private set => mDatabaseName = value; }
+        public string DatabaseUser { get => mDatabaseUser; private set => mDatabaseUser = value; }
+        public string DatabasePassword { get => mDatabasePassword; private set => mDatabasePassword = value; }
+    }
+}
--- Survey Configurator Development/SurveyConfiguratorDllProjects/SurveyConfiguratorEntities/Properties/AssemblyInfo.cs
+++ Survey Configurator Development/SurveyConfiguratorDllProjects/SurveyConfiguratorEntities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SurveyQuestions")]
+[assembly: AssemblyTitle("SurveyConfiguratorEntities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SurveyQuestions")]
+[assembly: AssemblyProduct("SurveyConfiguratorEntities")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7c56a4ba-d8ce-415e-9908-5f60f70761b9")]
+[assembly: Guid("374ac4a3-caee-4823-9019-61e38fa1f8d4")]
 
 // Version information for an assembly consists of the following four values:
 //
