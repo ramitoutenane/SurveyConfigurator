ea905e43be38490a885cfe071dc79ad6d54ba9ea
Rami <mhtoutenane119@cit.just.edu.jo>
2020-11-24 11:53:32 +0200

    New question configurator project using dlls

---
+++ SurveyConfiguratorApp/Main.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SurveyConfiguratorApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.questionDataGridView = new System.Windows.Forms.DataGridView();
+            this.QuestionTextColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QuestionTypeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.QuestionOrderColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.questionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.addButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.languageComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.questionDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.questionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            resources.ApplyResources(this.titleLabel, "titleLabel");
+            this.titleLabel.Name = "titleLabel";
+            // 
+            // questionDataGridView
+            // 
+            this.questionDataGridView.AllowDrop = true;
+            this.questionDataGridView.AllowUserToOrderColumns = true;
+            this.questionDataGridView.AutoGenerateColumns = false;
+            this.questionDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.questionDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.questionDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.questionDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.QuestionTextColumn,
+            this.QuestionTypeColumn,
+            this.QuestionOrderColumn});
+            this.questionDataGridView.DataSource = this.questionBindingSource;
+            resources.ApplyResources(this.questionDataGridView, "questionDataGridView");
+            this.questionDataGridView.MultiSelect = false;
+            this.questionDataGridView.Name = "questionDataGridView";
+            this.questionDataGridView.RowHeadersVisible = false;
+            this.questionDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.questionDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.questionDataGridView.StandardTab = true;
+            this.questionDataGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.questionDataGridView_CellDoubleClick);
+            this.questionDataGridView.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.questionDataGridView_ColumnHeaderMouseClick_1);
+            this.questionDataGridView.SelectionChanged += new System.EventHandler(this.questionDataGridView_SelectionChanged);
+            // 
+            // QuestionTextColumn
+            // 
+            this.QuestionTextColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.QuestionTextColumn.DataPropertyName = "Text";
+            this.QuestionTextColumn.FillWeight = 65F;
+            resources.ApplyResources(this.QuestionTextColumn, "QuestionTextColumn");
+            this.QuestionTextColumn.Name = "QuestionTextColumn";
+            this.QuestionTextColumn.ReadOnly = true;
+            this.QuestionTextColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // QuestionTypeColumn
+            // 
+            this.QuestionTypeColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.QuestionTypeColumn.DataPropertyName = "TypeString";
+            this.QuestionTypeColumn.FillWeight = 25F;
+            resources.ApplyResources(this.QuestionTypeColumn, "QuestionTypeColumn");
+            this.QuestionTypeColumn.Name = "QuestionTypeColumn";
+            this.QuestionTypeColumn.ReadOnly = true;
+            this.QuestionTypeColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // QuestionOrderColumn
+            // 
+            this.QuestionOrderColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.QuestionOrderColumn.DataPropertyName = "Order";
+            this.QuestionOrderColumn.FillWeight = 10F;
+            resources.ApplyResources(this.QuestionOrderColumn, "QuestionOrderColumn");
+            this.QuestionOrderColumn.Name = "QuestionOrderColumn";
+            this.QuestionOrderColumn.ReadOnly = true;
+            this.QuestionOrderColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // questionBindingSource
+            // 
+            this.questionBindingSource.DataSource = typeof(SurveyConfiguratorApp.Question);
+            // 
+            // addButton
+            // 
+            resources.ApplyResources(this.addButton, "addButton");
+            this.addButton.Name = "addButton";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // editButton
+            // 
+            resources.ApplyResources(this.editButton, "editButton");
+            this.editButton.Name = "editButton";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            resources.ApplyResources(this.deleteButton, "deleteButton");
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.BackgroundImage = global::SurveyConfiguratorApp.Properties.Resources.refresh;
+            resources.ApplyResources(this.refreshButton, "refreshButton");
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // languageComboBox
+            // 
+            this.languageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.languageComboBox.Items.AddRange(new object[] {
+            resources.GetString("languageComboBox.Items"),
+            resources.GetString("languageComboBox.Items1")});
+            resources.ApplyResources(this.languageComboBox, "languageComboBox");
+            this.languageComboBox.Name = "languageComboBox";
+            this.languageComboBox.SelectedIndexChanged += new System.EventHandler(this.languageComboBox_SelectedIndexChanged);
+            // 
+            // Main
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.languageComboBox);
+            this.Controls.Add(this.questionDataGridView);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.questionDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.questionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.DataGridView questionDataGridView;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeStringDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn orderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource questionBindingSource;
+        private System.Windows.Forms.ComboBox languageComboBox;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QuestionTextColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QuestionTypeColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn QuestionOrderColumn;
+    }
+}
+
